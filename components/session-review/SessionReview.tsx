0402f936b89d400b7495afb667647e36645174d0
namyagaur <namyaagaur@gmail.com>
2026-07-31 17:24:45 +0530

    updation

---
+++ components/session-review/SessionReview.tsx
@@ -0,0 +1,9 @@
+import { ReviewPageOne } from "./overview";
+
+export function SessionReview() {
+  return (
+    <div className="h-screen w-full overflow-hidden bg-white">
+      <ReviewPageOne />
+    </div>
+  );
+}
