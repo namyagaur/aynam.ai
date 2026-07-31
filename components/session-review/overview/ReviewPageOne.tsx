386bb4c62ec38c6198ecb033ddd791f967ff2335
namyagaur <namyaagaur@gmail.com>
2026-07-31 19:20:33 +0530

    update ⭐⭐⭐

--- components/session-review/overview/ReviewPageOne.tsx
+++ components/session-review/overview/ReviewPageOne.tsx
@@ -5,9 +5,9 @@ import { ReviewPagination } from "./ReviewPagination";
 
 export function ReviewPageOne() {
   return (
-<div className="mx-auto flex h-full w-full max-w-[1180px] flex-col">
-        <ReviewIntro />
-      <div className="mt-6">
+    <div className="mx-auto flex h-full w-full max-w-[980px] flex-col px-10 pt-4">
+      <ReviewIntro />
+      <div className="mt-3">
         <CommunicationProfileCard />
         <SessionHighlights />
         <ReviewPagination />
