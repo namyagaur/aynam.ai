d8fd15cb15b54f0de94fe90a6a0e7a60e6a2d4de
namyagaur <namyaagaur@gmail.com>
2026-07-31 17:51:08 +0530

    updateeeee

--- components/session-review/SessionReview.tsx
+++ components/session-review/SessionReview.tsx
@@ -2,7 +2,7 @@ import { ReviewPageOne } from "./overview";
 
 export function SessionReview() {
   return (
-    <div className="h-screen w-full overflow-hidden bg-white">
+    <div className="h-screen w-full overflow-hidden bg-[#FCFCFD]">
       <ReviewPageOne />
     </div>
   );
